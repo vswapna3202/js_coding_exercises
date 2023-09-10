a0c2671ade6eb5bb31c88230fda32666543f2bb5
Swapna V <vswapna3202@gmail.com>
2023-09-10 23:31:38 +0100

    feat: added code for exercise002.js

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,39 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	if (sandwich === undefined || !sandwich.fillings)
+	    throw new Error('ingredients is required');
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	if (person === undefined || !person.city)
+	    throw new Error('person is required');
+	return person.city === 'Manchester';
+
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	if (typeof people !== 'number' || isNaN(people) || people < 0)
+	    throw new Error ('people needs to be a number');
+	const capacity = 40;
+	const buses = people / capacity;
+	const roundedBuses = (buses % 1 === 0) ? buses : Math.floor(buses) + 1;
+	return roundedBuses;
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	if (arr === undefined || arr.length === 0) throw new Error('arr is required');
+	let sheepCount = 0;
+	for (let i = 0; i < arr.length; i++){
+	    if (arr[i] === 'sheep') sheepCount += 1;
+	}
+	return sheepCount;
+
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	if (person === undefined || !person.address.postCode) throw new Error('person is required');
+	if (typeof person.address.postCode !== 'string') throw new Error('postcode should be string');
+	return (person.address.postCode.charAt(0) === 'M'
+	        && person.address.city === 'Manchester');
 }

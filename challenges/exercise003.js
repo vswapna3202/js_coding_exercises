a56b877c82ad217bd50e5b545ca453e9328f52ee
Swapna V <vswapna3202@gmail.com>
2023-09-11 22:23:21 +0100

    feat: fixed eslint changed on exercise003.js

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -2,7 +2,7 @@ export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
 	let squares = []
 	for(let i=0;i<nums.length;i++){
-	    squares.push(nums[i] * nums[i]);
+        squares.push(nums[i] * nums[i]);
 	}
 	return squares;
 }
@@ -11,21 +11,19 @@ export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
 	let camelCaseWords = "";
 	for (let i = 0; i < words.length; i++){
-	    if( i !== 0)
-	        camelCaseWords = camelCaseWords+words[i].charAt(0).toUpperCase()+words[i].slice(1);
+        if( i !== 0)
+            camelCaseWords = camelCaseWords+words[i].charAt(0).toUpperCase()+words[i].slice(1);
         else
-	        camelCaseWords = words[i];
+            camelCaseWords = words[i];
 	}
 	return camelCaseWords;
 }
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
-	let totalSubjects = 0;
-
 	return people.reduce((totalSubjects, person) => {
-	    return totalSubjects + person.subjects.filter(subject => subject !== "").length;
-	    },0);
+        return totalSubjects + person.subjects.filter(subject => subject !== "").length;
+    },0);
 }
 
 export function checkIngredients(menu, ingredient) {
@@ -34,9 +32,9 @@ export function checkIngredients(menu, ingredient) {
 
 	for (let i = 0; i < menu.length; i++){
 
-	    if(menu[i].ingredients.includes(ingredient)){
-	        return true;
-	    }
+        if(menu[i].ingredients.includes(ingredient)){
+            return true;
+        }
 	}
 	return false;
 }
@@ -46,9 +44,9 @@ export function duplicateNumbers(arr1, arr2) {
 	if (arr2 === undefined) throw new Error('arr2 is required');
 	let duplicateArray = [];
 	for(let i = 0; i < arr1.length; i++){
-	    if(arr2.includes(arr1[i]) && !duplicateArray.includes(arr1[i])){
-	        duplicateArray.push(arr1[i]);
-	    }
+        if(arr2.includes(arr1[i]) && !duplicateArray.includes(arr1[i])){
+            duplicateArray.push(arr1[i]);
+        }
 	}
 	return duplicateArray.sort((a, b) => a - b);
 }

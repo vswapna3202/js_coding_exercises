ed30356613a17382ce6feca2df612a96261e64f4
Swapna V <vswapna3202@gmail.com>
2023-09-11 23:22:01 +0100

    feat: added code for exercise004.js

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,41 +1,64 @@
 export function findSmallNums(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+    const newNumArray = nums.filter(function(item) {
+        return item < 1;
+    });
+    return newNumArray;
 }
 
 export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
-	// Your code here
+	const newNamesArray = names.filter(function(item){
+        return (item.charAt(0) === char)
+	});
+	return newNamesArray;
 }
 
 export function findVerbs(words) {
 	if (!words) throw new Error('words is required');
-	// Your code here
+    const verbsArray = words.filter(function(item){
+        return (item.substring(0,3) === 'to ')
+    });
+    return verbsArray;
 }
 
 export function getIntegers(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+	const integerArray = nums.filter(function(item){
+        return Number.isInteger(item);
+	});
+	return integerArray;
 }
 
 export function getCities(users) {
 	if (!users) throw new Error('users is required');
-	// Your code here
+	const cityArray = users.filter(user => user.data && user.data.city && user.data.city.displayName).map(user => user.data.city.displayName);
+	return cityArray;
 }
 
 export function getSquareRoots(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+    const squareRootsArray = nums.map(function(item){
+        return parseFloat(Math.sqrt(item).toFixed(2));
+    });
+    return squareRootsArray;
 }
 
 export function findSentencesContaining(sentences, str) {
 	if (!sentences) throw new Error('sentences is required');
 	if (!str) throw new Error('str is required');
-	// Your code here
+	str = str.toLowerCase();
+	const matchingSentence = sentences.filter(function(sentence){
+        const lowerCaseSentence = sentence.toLowerCase();
+        return lowerCaseSentence.includes(str);
+	});
+	return matchingSentence;
 }
 
 export function getLongestSides(triangles) {
 	if (!triangles) throw new Error('triangles is required');
-	// Your code here
+	const longestSide = triangles.map(triangle => Math.max(...triangle));
+	return longestSide;
+
 }

953a55cde79a7f29fd37227db8150748a5a484ea
Swapna V <vswapna3202@gmail.com>
2023-09-13 11:28:23 +0100

    feat: added code for exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -29,21 +29,42 @@ export const reverseNumber = (n) => {
 
 export const sumArrays = (arrs) => {
 	if (arrs === undefined) throw new Error('arrs is required');
-	// Your code here!
+	let total = 0;
+	for(let i = 0; i < arrs.length; i++){
+	    for(let j = 0; j < arrs[i].length; j++){
+	        total = total + arrs[i][j];
+	    }
+	}
+	return total;
 };
 
 export const arrShift = (arr) => {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	if (arr.length < 2) return arr;
+	[arr[0],arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+	return arr;
 };
 
 export const findNeedle = (haystack, searchTerm) => {
 	if (haystack === undefined) throw new Error('haystack is required');
 	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+	const values = Object.values(haystack);
+	const ifFound = values.some(item => typeof item === 'string' &&
+	    item.toLowerCase().includes(searchTerm.toLowerCase()));
+	return ifFound;
 };
 
 export const getWordFrequencies = (str) => {
 	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+	const strArray = str.split(' ');
+	const frequencies = {};
+	strArray.forEach(word => {
+	    const cleanedWord = word.replace(/[^\w\s]/g, '').toLowerCase();
+	    if(frequencies[cleanedWord]){
+	        frequencies[cleanedWord]++;
+	    }else{
+	        frequencies[cleanedWord]=1;
+	    }
+	});
+	return frequencies;
 };

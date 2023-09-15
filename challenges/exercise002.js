3bba4972887d7075f643d785bd1b205bcf38a122
Swapna V <vswapna3202@gmail.com>
2023-09-15 13:42:18 +0100

    refactored exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,12 +1,12 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined || !sandwich.fillings)
-        throw new Error('ingredients is required');
+        throw new Error('sandwich and fillings are required');
 	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined || !person.city)
-        throw new Error('person is required');
+        throw new Error('person and city person is from is required');
 	return person.city === 'Manchester';
 
 }
@@ -22,17 +22,21 @@ export function getBusNumbers(people) {
 }
 
 export function countSheep(arr) {
-	if (arr === undefined || arr.length === 0) throw new Error('arr is required');
+	if (arr === undefined || arr.length === 0)
+        throw new Error('arr is required');
 	let sheepCount = 0;
 	for (let i = 0; i < arr.length; i++){
-        if (arr[i] === 'sheep') sheepCount += 1;
+        if (arr[i] === 'sheep')
+            sheepCount += 1;
 	}
 	return sheepCount;
-
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined || !person.address.postCode) throw new Error('person is required');
-	if (typeof person.address.postCode !== 'string') throw new Error('postcode should be string');
-	return (person.address.postCode.charAt(0) === 'M' && person.address.city === 'Manchester');
+	if (person === undefined || !person.address.postCode)
+        throw new Error('person is required');
+	if (typeof person.address.postCode !== 'string')
+        throw new Error('postcode should be string');
+	return (person.address.postCode.charAt(0) === 'M'
+        && person.address.city === 'Manchester');
 }

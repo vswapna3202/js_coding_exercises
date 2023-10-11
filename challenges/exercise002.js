bf2562749bea8ac0f9209056be56d3b9483b4485
Swapna V <vswapna3202@gmail.com>
2023-10-11 18:34:50 +0100

    Refactor: Refactored code for constants

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -13,20 +13,19 @@ export function isFromManchester(person) {
 }
 
 export function getBusNumbers(people) {
-    const BUS_CAPACITY = 40;
 	if (people === undefined) throw new Error('people is required');
 	if (typeof people !== 'number' || people < 0)
         throw new Error ('people needs to be a number');
-
+    const BUS_CAPACITY = 40;
 	const buses = people / BUS_CAPACITY;
 	const roundedBuses = (buses % 1 === 0) ? buses : Math.floor(buses) + 1;
 	return roundedBuses;
 }
 
 export function countSheep(arr) {
-    const ARRAY_ITEM = 'sheep';
 	if (arr === undefined || arr.length === 0)
         throw new Error('arr is required');
+    const ARRAY_ITEM = 'sheep';
 	let sheepCount = 0;
 	for (let i = 0; i < arr.length; i++){
         if (arr[i] === ARRAY_ITEM)
@@ -36,12 +35,12 @@ export function countSheep(arr) {
 }
 
 export function hasMPostCode(person) {
-    const TARGET_CITY = 'Manchester';
-    const POST_CODE_START = 'M';
 	if (person === undefined || !person.address.postCode)
         throw new Error('person is required');
 	if (typeof person.address.postCode !== 'string')
         throw new Error('postcode should be string');
+    const TARGET_CITY = 'Manchester';
+    const POST_CODE_START = 'M';
 	return (person.address.postCode.charAt(0) === POST_CODE_START
         && person.address.city === TARGET_CITY);
 }
